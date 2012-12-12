b423f4a485f2e8ae3e4bb5e4d5562a59cebe1481
Dirk Olbertz <dirk.olbertz@gmail.com>
2012-12-12 11:39:29 +0100

    Downgrade einer Anpassung für ein neueres Doctrine

--- src/Knp/Component/Pager/Event/Subscriber/Paginate/Doctrine/ORM/QuerySubscriber.php
+++ src/Knp/Component/Pager/Event/Subscriber/Paginate/Doctrine/ORM/QuerySubscriber.php
@@ -81,7 +81,19 @@ class QuerySubscriber implements EventSubscriberInterface
                         ->setFirstResult(null)
                         ->setMaxResults(null)
                     ;
-                    $whereInQuery->setParameter(WhereInWalker::PAGINATOR_ID_ALIAS, $ids);
+                    $type = $limitSubQuery->getHint($useDoctrineWalkers ?
+                        DoctrineLimitSubqueryWalker::IDENTIFIER_TYPE :
+                        LimitSubqueryWalker::IDENTIFIER_TYPE
+                    );
+                    $idAlias = $useDoctrineWalkers ? DoctrineWhereInWalker::PAGINATOR_ID_ALIAS : WhereInWalker::PAGINATOR_ID_ALIAS;
+                    foreach ($ids as $i => $id) {
+                        $whereInQuery->setParameter(
+                            $idAlias . '_' . ++$i,
+                            $id,
+                            $type->getName()
+                        );
+                    }
+
                     $result = $whereInQuery->execute();
                 } else {
                     $event->target
